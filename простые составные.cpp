c2bc5c44235cc05b8c525d56b158fff74ea4aca4
“Alena” <alenapaskina90@gmail.com>
2022-10-31 22:11:51 +0500

    Первая загрузка

---
+++ простые составные.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+
+using namespace std;
+
+bool isPrime(int number)
+{
+    for (int i = 2; i < number; i++)
+    {
+        if (number % i == 0)
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+int main()
+{
+    int number;
+    cout << "Enter a number: ";
+    cin >> number;
+
+    if (isPrime(number) == true)
+    {
+        cout << "The number " << number << " is prime" << endl; //число простое
+    }
+    else
+    {
+        cout << "The number " << number << " is composite" << endl; //число составное 
+    }
+}
